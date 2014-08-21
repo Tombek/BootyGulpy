8583f596f1df8966d26d76a63bb7ecd44cfaeee0
Cyril Krylatov <cyril@colorz.fr>
2014-08-21 15:01:58 +0200

    Gulp: Compile bootstrap, core, watch core

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    watch = require('gulp-watch'),
+    less = require('gulp-less'),
+    concat = require("gulp-concat-css"),
+    minifyCss = require("gulp-minify-css");
+
+// Bootstrap : Compile Bootstrap only
+
+gulp.task('compile-bootstrap', function() {
+    gulp.src('assets/less/bootstrap/less/bootstrap.less') 
+        .pipe(less())
+        .pipe(concat('bootstrap.prod.min.css')) 
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist'));
+});
+
+// Core : compile, concat & minify
+
+gulp.task('compile-concat-minify', function() {
+    gulp.src('assets/less/core/*.less') 
+        .pipe(less())
+        .pipe(concat('prod.min.css')) 
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist'));
+});
+
+// Core : watch
+
+gulp.task('watch-core-css', function() {
+    watch({
+        glob: 'assets/less/core/*.less'
+    }, function(files) {
+        gulp.start('compile-concat-minify');
+    });
+});
